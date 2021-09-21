d36a80dda5da6438109b5ed66c66668d5eeb1b66
jefferson1104 <jefferson1104junior@gmail.com>
2021-09-21 12:38:39 -0300

    webhook stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
+
 import { stripe } from "../../services/stripe";
 import { saveSubscription } from "./_lib/manageSubscription";
 
@@ -37,12 +38,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
+      console.log('ERRO', err)
       return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
     const { type } = event;
 
-    if (!relevantEvents.has(type)) {
+    if (relevantEvents.has(type)) {
       try {
         switch (type) {
           case 'checkout.session.completed':
@@ -58,7 +60,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             throw new Error('Unhandled event.')
         }
       } catch (err) {
-        return res.json({ error: 'Webhook handler failed.'})
+        return res.status(400).json({ error: 'Webhook handler failed' })
       }
     }
 
