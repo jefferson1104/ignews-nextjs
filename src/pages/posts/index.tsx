a1e1c773f231c2a5a38ee4bf6e6df6d9db86db9e
jefferson1104 <jefferson1104junior@gmail.com>
2021-09-25 12:10:59 -0300

    Listando os posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,9 +2,20 @@ import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,21 +24,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>21 de abril de 2021</time>
-            <strong>Creating monorepo application</strong>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.</p>
-          </a>
-          <a href='#'>
-            <time>21 de abril de 2021</time>
-            <strong>Creating monorepo application</strong>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.</p>
-          </a>
-          <a href='#'>
-            <time>21 de abril de 2021</time>
-            <strong>Creating monorepo application</strong>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -42,13 +45,24 @@ export const getStaticProps: GetStaticProps = async () => {
   ], {
     fetch: ['post.title', 'post.content'],
     pageSize: 100,
-  })
+  });
 
-  console.log('CONSOLANDO POSTS:', response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
 
   return {
     props: {
-
+      posts
     }
   }
 }
