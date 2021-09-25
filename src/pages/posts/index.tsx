8b4f6dad738b430d3428788e6c344be450bab77a
jefferson1104 <jefferson1104junior@gmail.com>
2021-09-25 11:57:02 -0300

    Configurando e consumindo api do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -30,3 +33,23 @@ export default function Posts() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response =  await prismic.query([
+    Prismic.predicates.at('document.type', 'pos')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  console.log('CONSOLANDO POSTS:', response)
+
+  return {
+    props: {
+
+    }
+  }
+}
+
